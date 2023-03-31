32c328f518542cda7517995751e1b20f535db5dd
Matias Pequeno <matias.pequeno@gmail.com>
2023-03-31 18:09:16 -0300

    Call lookup from cli

--- ators/src/main.rs
+++ ators/src/main.rs
@@ -1,18 +1,21 @@
 mod cli;
 
 use anyhow::Result;
-use atorsl::{data::Context, load_dwarf, load_object, read::Dump};
+use atorsl::{
+    data::{Context, ObjectExt},
+    load_dwarf, load_object,
+    read::Lookup,
+};
 use cli::FromArgs;
 
 fn main() -> Result<()> {
-    let mmap;
-    let cow;
     let context = Context::from_args(cli::build().get_matches());
-    let object = load_object!(context.object_path, mmap)?;
-    let dwarf = load_dwarf!(object, cow);
 
-    object
-        .dump()?
+    let (mmap, cow);
+    let object = load_object!(context.object_path, mmap)?;
+
+    load_dwarf!(object, cow)
+        .lookup(object.vmaddr()?, context)?
         .into_iter()
         .for_each(|symbol| println!("{symbol}"));
 
