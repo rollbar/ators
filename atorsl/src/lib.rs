d33bc66cd3a7edaf82eeabaaeaf552d7d51c8c14
Matias Pequeno <matias.pequeno@gmail.com>
2023-04-01 20:42:54 -0300

    Moved load macros back to lib

--- atorsl/src/lib.rs
+++ atorsl/src/lib.rs
@@ -2,13 +2,46 @@ pub mod addr;
 pub mod context;
 pub mod error;
 pub mod ext;
-pub mod load;
 pub mod lookup;
 
 pub use addr::Addr;
 pub use context::Context;
 pub use error::Error;
 pub use lookup::Symbolicate;
+
+#[macro_export]
+macro_rules! load_object {
+    ($path:expr, $binding:ident) => {{
+        $binding = unsafe { memmap2::Mmap::map(&std::fs::File::open(&$path)?) }?;
+        Result::<object::File, atorsl::Error>::Ok(object::File::parse(&*$binding)?)
+    }};
+}
+
+#[macro_export]
+macro_rules! load_dwarf {
+    ($object:expr, $binding:ident) => {{
+        $binding = gimli::Dwarf::load(|sid| -> gimli::Result<std::borrow::Cow<[u8]>> {
+            Ok(
+                <object::File as object::Object>::section_by_name(&$object, sid.name())
+                    .and_then(|s| {
+                        <object::Section as object::ObjectSection>::uncompressed_data(&s).ok()
+                    })
+                    .unwrap_or(std::borrow::Cow::Borrowed(&[][..])),
+            )
+        })?;
+
+        $binding.borrow(|section| {
+            gimli::EndianSlice::new(
+                &*section,
+                if <object::File as object::Object>::is_little_endian($object) {
+                    gimli::RunTimeEndian::Little
+                } else {
+                    gimli::RunTimeEndian::Big
+                },
+            )
+        })
+    }};
+}
 
 use gimli::{EndianSlice, RunTimeEndian};
 
