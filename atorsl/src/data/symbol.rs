fa311f1a13e73f00d31b9b13d21e323dfb662c15
Matias Pequeno <matias.pequeno@gmail.com>
2023-04-22 16:44:27 -0300

    Don't derive ordering from symbols

--- atorsl/src/data/symbol.rs
+++ atorsl/src/data/symbol.rs
@@ -2,14 +2,14 @@ use super::Addr;
 use itertools::Either;
 use std::path::PathBuf;
 
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SourceLoc {
     pub file: PathBuf,
     pub line: u16,
     pub col: Option<u16>,
 }
 
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Symbol {
     pub name: String,
     pub loc: Either<SourceLoc, Addr>,
