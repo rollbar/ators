0c0deb5b104b86b6f760030c0ae4626f7dbdae83
Matias Pequeno <matias.pequeno@gmail.com>
2023-04-26 17:14:51 -0300

    Generate atos output

--- ators/build.rs
+++ ators/build.rs
@@ -3,49 +3,73 @@ use memmap2::Mmap;
 use object::Object;
 use std::{
     env, fs,
-    io::{self, Write},
+    io::{self, BufRead, Write},
     path::PathBuf,
+    process::Command,
 };
 
+const SYMBOLICATE: &str = "/usr/bin/atos";
 const TEST_DWARF: &str = "test.dwarf";
+const TEST_ADDRS: &str = "test_addrs.txt";
+const TEST_SYMR: &str = "test_symr.txt";
 
 fn main() -> Result<()> {
-    println!("cargo:rerun-if-changed=../fixtures/test_addrs.txt");
+    println!("cargo:rerun-if-changed={}", path(TEST_ADDRS)?.display());
+    println!("cargo:rerun-if-changed={}", path(TEST_SYMR)?.display());
 
-    test_addrs_path()?
+    path(TEST_ADDRS)?
         .exists()
         .eq(&false)
         .then(generate_test_addrs_file);
 
+    path(TEST_SYMR)?
+        .exists()
+        .eq(&false)
+        .then(generate_test_symr_file);
+
     Ok(())
 }
 
-fn pwd() -> Result<String, env::VarError> {
-    env::var("CARGO_MANIFEST_DIR")
-}
-
-fn test_addrs_path() -> Result<PathBuf, env::VarError> {
-    Ok([&pwd()?, "..", "fixtures", "test_addrs.txt"]
+fn path(file: &str) -> Result<PathBuf> {
+    Ok([&env::var("CARGO_MANIFEST_DIR")?, "..", "fixtures", file]
         .iter()
         .collect())
 }
 
 fn generate_test_addrs_file() -> Result<()> {
-    let path = [&pwd()?, "..", "fixtures", "objects", TEST_DWARF]
-        .iter()
-        .collect::<PathBuf>();
-
-    if !path.exists() {
+    if !path(TEST_DWARF)?.exists() {
         Err(anyhow!("Can't generate test addrs without test.dwarf"))?;
     }
 
-    let mmap = unsafe { Mmap::map(&fs::File::open(path)?) }?;
+    let mmap = unsafe { Mmap::map(&fs::File::open(path(TEST_DWARF)?)?) }?;
     let object = object::File::parse(&*mmap)?;
-    let mut addrs_file = io::BufWriter::new(fs::File::create(test_addrs_path()?)?);
+    let mut addrs_file = io::BufWriter::new(fs::File::create(path(TEST_ADDRS)?)?);
 
     for symbol in object.symbol_map().symbols() {
         write!(addrs_file, "{:#018x} ", symbol.address())?;
     }
 
     Ok(addrs_file.flush()?)
+}
+
+fn generate_test_symr_file() -> Result<()> {
+    let output = Command::new(SYMBOLICATE)
+        .args([
+            "-i",
+            "-o",
+            &path(TEST_DWARF)?.to_string_lossy(),
+            "-s",
+            "0",
+            "-f",
+            &path(TEST_ADDRS)?.to_string_lossy(),
+        ])
+        .output()?;
+
+    let mut symr_file = io::BufWriter::new(fs::File::create(path(TEST_SYMR)?)?);
+
+    for symbol in output.stdout.lines() {
+        writeln!(symr_file, "{}", symbol?)?;
+    }
+
+    Ok(symr_file.flush()?)
 }
